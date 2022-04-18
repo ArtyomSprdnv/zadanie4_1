86f0e7a38bda7e652fbed54dceda79abaf40c0a8
ArtyomSprdnv <92688941+ArtyomSprdnv@users.noreply.github.com>
2022-04-18 10:30:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -166,8 +166,8 @@ else {
 
 // Сохранение в базу данных.
 
-$user = 'u41026';
-$pass = '4433573';
+$user = 'u47502';
+$pass = '8701243';
 $db = new PDO('mysql:host=localhost;dbname=u41026', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 
 try {
